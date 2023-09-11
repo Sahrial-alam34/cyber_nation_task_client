6c640ec12f7eeec0b64ad212a815c9f6f4263a4e
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-09-11 19:00:57 +0600

    footer update

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { MdOutlineLocationOn,MdOutlineEmail } from "react-icons/md";
+import { IoCallOutline } from "react-icons/io5";
+import { AiOutlineHome } from "react-icons/ai";
+import { RiStarHalfFill } from "react-icons/ri";
 import './Footer.css'
 
 const Footer = () => {
@@ -7,7 +10,7 @@ const Footer = () => {
         <footer >
             <div className="footer p-10 bg-gray-950 text-white">
         
-                <div className="">
+                <div className="footer-menu">
                     <span className="footer-title border-bottom-x ">ফুটার মেনু </span>
                     
                    <span className="border-list-bottom-x"> +<a className="link link-hover"> প্রতিষ্ঠানের ইতিহাস</a></span>
@@ -17,25 +20,28 @@ const Footer = () => {
                     <span className="border-list-bottom-x">+ <a className="link link-hover">কৃতি শিক্ষার্থী</a></span>
                     <span className="border-list-bottom-x">+ <a className="link link-hover">নোটিশ</a></span>
                 </div>
-                <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                <div className="">
+                    <span className="footer-title border-bottom-x ">ফুটার মেনু </span>
+                    
+                   <span className="border-list-bottom-x"> +<a className="link link-hover"> পরীক্ষার ফলাফল</a></span>
+                    <span className="border-list-bottom-x">+ <a className="link link-hover">ব্লগ</a></span>
+                    <span className="border-list-bottom-x">+ <a className="link link-hover"> ডাউনলোড</a></span>
+                    <span className="border-list-bottom-x">+ <a className="link link-hover">পরীক্ষার রুটিন</a></span>
+               
+                    <span className="border-list-bottom-x">+ <a className="link link-hover">ভর্তি</a></span>
+                    <span className="border-list-bottom-x">+ <a className="link link-hover">বাংলা টিউটোরিয়াল</a></span>
                 </div>
-                <div>
-                    <span className="footer-title">Social Media</span>
-                    <a className="link link-hover"><FaFacebook className='h-[20px] w-[60px]' /></a>
-                    <a className="link link-hover"><FaInstagram className='h-[20px] w-[60px]' /></a>
-                    <a className="link link-hover"><FaTwitter className='h-[20px] w-[60px]' /></a>
+                <div className="">
+                    <span className="footer-title border-bottom-x ">যোগাযোগের ঠিকানা</span>
+                    
+                   <span className=" flex gap-2"><MdOutlineLocationOn  className="text-2xl"></MdOutlineLocationOn> <p className=""> চুনারুঘাট সদর, উপজেলা : চুনারুঘাট, জেলা : হবিগঞ্জ,<br /> বিভাগ : সিলেট, বাংলাদেশ</p></span>
+                   <span className=" flex gap-2"><IoCallOutline className="text-xl"></IoCallOutline> <p className=""> +88020000000</p></span>
+                   <span className=" flex gap-2"><MdOutlineEmail className="text-xl"></MdOutlineEmail> <p className=""> info@aboutme.com</p></span>
+                   <span className=" flex gap-2"><RiStarHalfFill className="text-xl"></RiStarHalfFill> <p className=""> ইআইআইএন নং : ২৩২৩২৩</p></span>
+                   <span className=" flex gap-2"><AiOutlineHome className="text-xl"></AiOutlineHome> <p className=""> কলেজ কোড : ১৩২১২</p></span>
+                  
                 </div>
-                <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
-                </div>
+           
             </div>
             <div className=" footer-center p-4 bg-gray-800 text-white">
                 <div className="flex justify-between">
